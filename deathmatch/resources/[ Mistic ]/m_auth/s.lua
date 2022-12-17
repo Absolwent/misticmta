c23606f21a227fd1009b5f09b6c313f37d2e071a
Karol Lewandowski <120670635+Absolwent@users.noreply.github.com>
2022-12-17 17:40:21 +0100

    Code improvement

--- deathmatch/resources/[ Mistic ]/m_auth/s.lua
+++ deathmatch/resources/[ Mistic ]/m_auth/s.lua
@@ -4,23 +4,18 @@ local minutes = time.minute
 local seconds = time.second
 local timeFormated = string.format("Local Time: %02d:%02d:%02d",  hours, minutes, seconds)
 
-addEvent("logging:checkAccount", true)
-addEventHandler("logging:checkAccount", resourceRoot, function(login,pass)
-local result=exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE login=?", login)
+--[[
+SETTINGS
+--]]
+local limit = 2 -- Limit kont na jednym serialu
+
+
+-- Nie  testowane
+addEventHandler("auth:check", resourceRoot, function(login,pass)
+local result = exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE login=?", login)
+local pSerial = getPlayerSerial(player)
 if result and #result > 0 then
-    if result[1].login == login and result[1].pass == md5(pass) then
-        local query=exports["m_mysql"]:dbSet("UPDATE m_users SET pass=? WHERE login=?",teaEncode(pass,"Trujeczka"),login)
-	return
-    end
-    if result[1].login == login and result[1].pass == teaEncode(pass,"Ryjek") then
-        local query=exports["m_mysql"]:dbSet("UPDATE m_users SET pass=? WHERE login=?",teaEncode(pass,"Trujeczka"),login)
-        return
-    end
-    if result[1].login == login and result[1].pass == teaEncode(pass,"Trujeczka") then
-        local query=exports["m_mysql"]:dbSet("UPDATE m_users SET pass=? WHERE login=?",teaEncode(pass,"profesionalnekodowanie"),login)
-        return
-    end
-    if result[1].login == login and result[1].pass == teaEncode(pass,"profesionalnekodowanie") then
+    if result[1].login == login and result[1].pass == teaEncode(pass,"MD5_PRZYJACIEL_PSEUDOLA") then
     for i,player in pairs(getElementsByType("player")) do
         if getElementData(player,"user:id") == result[1].id then
             triggerEvent("notiAdd", client, "error", "Ten gracz już gra!")
@@ -41,7 +36,7 @@ if result and #result > 0 then
         removeElementData(client, "soundDisabled")
         removeElementData(client, "soundEnabled")
         if result[1].serial == false then
-            local query=exports["m_mysql"]:dbSet("UPDATE m_users SET serial=? WHERE login=?",getPlayerSerial(client),login)
+            local query=exports["m_mysql"]:dbSet("UPDATE m_users SET serial=? WHERE login=?",pSerial,login)
         end
         else
             triggerEvent("notiAdd", client, "error", "Podano błędne dane")
@@ -50,19 +45,18 @@ if result and #result > 0 then
             triggerEvent("notiAdd", client, "error", "Ten login już instnieje!")
 	end
 end)
+addEvent("auth:check", true)
 
-local maks_ilosc_kont = 2
-addEvent("logging:newAccount", true)
-addEventHandler("logging:newAccount", resourceRoot, function(regLogin,regPass,regEmail)
-local result=exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE serial=?", getPlayerSerial(client))
-if result and #result >= maks_ilosc_kont then
+addEventHandler("auth:register", resourceRoot, function(regLogin,regPass,regEmail)
+local result = exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE serial=?", getPlayerSerial(client))
+if result and #result >= limit then
     triggerEvent("notiAdd", client, "error", "Przekroczyłeś(aś) limit kont na serwerze!")
     return end
-    local result=exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE login=?", regLogin)
+    local result = exports["m_mysql"]:dbGet("SELECT * FROM m_users WHERE login=?", regLogin)
     if result and #result > 0 then
         exports["m_notifications"]:addBox(client, "error", "Ten gracz już instnieje w bazie danych, wymyśl inną nazwę.")
     else
-        local query=exports["m_mysql"]:dbSet("INSERT INTO m_users (login,pass,remember,serial,email,changedpw) VALUES (?,?,?,?,?,??)", regLogin, teaEncode(regPass,"profesionalnekodowanie"),regPass,getPlayerSerial(client),regEmail,1)
+        local query = exports["m_mysql"]:dbSet("INSERT INTO m_users (login,pass,remember,serial,email,changedpw) VALUES (?,?,?,?,?,??)", regLogin, teaEncode(regPass,"MD5_PRZYJACIEL_PSEUDOLA"),regPass,pSerial,regEmail,1)
         if query then
             triggerEvent("notiAdd", client, "success", "Pomyślnie utworzono konto")
             setElementData(client, "newPlayer", true)
@@ -73,4 +67,7 @@ if result and #result >= maks_ilosc_kont then
         end
     end
 end)
+addEvent("auth:register", true)
+
+-- ostatnia aktualizacja: ( 17.12.2022 17:14 )
 
