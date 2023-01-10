07dc416d81c63bd1a042e4328976b3a4e7e757d3
Karol Lewandowski <120670635+Absolwent@users.noreply.github.com>
2023-01-10 20:36:45 +0100

    Removed hud from repository
    We don't want problems with authors

--- deathmatch/resources/[ Mistic ]/[ Interface ]/m_hud/fileS.lua
+++
@@ -1,8 +0,0 @@
-
-
-
-addCommandHandler("auto",function(plr,cmd,model)
-    local x,y,z = getElementPosition(plr)
-    x = exports["un-vehicles"]:createVehicle( tonumber(model), x,y+2,z)
-    exports["un-vehicles"]:saveVehicle(x)
-end)
